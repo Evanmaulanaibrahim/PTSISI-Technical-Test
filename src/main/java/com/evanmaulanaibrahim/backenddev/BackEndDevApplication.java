9c9aaf46841f982e9d9949264b259636215a3712
Evanmaulanaibrahim <evanmaulanaibrahim@gmail.com>
2024-10-25 19:18:03 +0700

    Initialize Project

---
+++ src/main/java/com/evanmaulanaibrahim/backenddev/BackEndDevApplication.java
@@ -0,0 +1,13 @@
+package com.evanmaulanaibrahim.backenddev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evanmaulanaibrahim/backenddev/BackEndDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evanmaulanaibrahim.backenddev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
